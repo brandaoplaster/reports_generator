57a4e87fc1fc95e3ba100432a45b6b7e8a2a51e4
brandaoplaster <brandaoplaster@gmail.com>
2021-03-25 23:33:08 -0300

    feat: adding function for sum values

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,14 +1,15 @@
 defmodule ReportsGenerator do
+  alias ReportsGenerator.Parser
+
   def build(filename) do
-    "reports/#{filename}"
-    |> File.stream!()
-    |> Enum.map(&parse_line(&1))
+    filename
+    |> Parser.parse_line()
+    |> Enum.reduce(report_acc(), fn line, report ->
+      sum_values(line, report)
+    end)
   end
 
-  def parse_line(line) do
-    line
-    |> String.trim()
-    |> String.split(",")
-    |> List.update_at(2, &String.to_integer/1)
-  end
+  defp sum_values([id, _food, price], report), do: Map.put(report, id, report[id] + price)
+
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
